040def3316064234a8c57144e7eff8ca831ba280
kamciekus <kazimieras.maciekus@gmail.com>
2021-12-22 14:22:58 +0200

    created client htmls

---
+++ client/scripts/variables.js
@@ -0,0 +1,8 @@
+//--------------------------URLs-----------------------------------
+export const URL_USERS = "http://localhost:8080/users";
+export const URL_ACCOUNTS = "http://localhost:8080/accounts";
+export const URL_BILLS = "http://localhost:8080/bills";
+//--------------------------FORMS----------------------------------
+export const logInForm = document.getElementById("logIn");
+export const registrationForm = document.getElementById("registration");
+export const addGroupForm = document.getElementById("addGroup");
